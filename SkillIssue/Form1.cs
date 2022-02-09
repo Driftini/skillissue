302d31da8c2aa679c10510acb572ab30cbf197d7
Driftini <yurisilveos741@gmail.com>
2022-02-09 22:29:05 +0100

    First commit

---
+++ SkillIssue/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SkillIssue
+{
+    public partial class Form1 : Form
+    {
+        Timer renderTimer;
+        GameLoop gameLoop;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Initialize Paint Event
+            Paint += Form1_Paint;
+            // Initialize renderTimer
+            renderTimer = new Timer();
+            renderTimer.Interval = 1000 / 120;
+            renderTimer.Tick += GraphicsTimer_Tick;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Optimization
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+            Size _GameRes = new Size(640, 360);
+
+            // Set form size according to GameRes
+            ClientSize = _GameRes;
+
+            // Initialize Game
+            Game SkilIssue = new Game();
+            SkilIssue.Resolution = _GameRes;
+
+            // Initialize and start the gameloop
+            gameLoop = new GameLoop();
+            gameLoop.Load(SkilIssue);
+            gameLoop.Start();
+
+            // Start the rendering timer
+            renderTimer.Start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Render the graphic buffer on the form
+            gameLoop.Draw(e.Graphics);
+        }
+
+        private void GraphicsTimer_Tick(object sender, EventArgs e)
+        {
+            // Refresh Form1 graphics
+            Invalidate();
+        }
+    }
+}
---
+++ SkillIssue/GameLoop.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace SkillIssue
+{
+    class GameLoop
+    {
+        private Game _SkillIssue;
+
+        /// <summary>
+        /// Whether or not the gameloop is running.
+        /// </summary>
+        public bool Running { get; private set; }
+
+        /// <summary>
+        /// Timer to reset the FPS counter.
+        /// </summary>
+        public Timer FPStimer = new Timer(1000);
+
+        /// <summary>
+        /// Load game into the gameloop.
+        /// </summary>
+        public void Load(Game gameObj)
+        {
+            _SkillIssue = gameObj;
+        }
+
+        /// <summary>
+        /// Start the gameloop.
+        /// </summary>
+        public async void Start()
+        {
+            if (_SkillIssue == null) throw new ArgumentException("Game not loaded!");
+
+            // Load game content
+            _SkillIssue.Load();
+
+            // Set gameloop state
+            Running = true;
+
+            // Start FPS timer
+            FPStimer.Elapsed += ResetFPS;
+            FPStimer.AutoReset = true;
+            FPStimer.Enabled = true;
+
+            // Set previous game time
+            DateTime _previousGameTime = DateTime.Now;
+
+            while (Running)
+            {
+                // Calculate the time elapsed since the last game loop cycle
+                TimeSpan GameTime = DateTime.Now - _previousGameTime;
+                // Update the current previous game time
+                _previousGameTime = _previousGameTime + GameTime;
+                // Update the game
+                _SkillIssue.Update(GameTime);
+                _SkillIssue.GameTime = GameTime.ToString();
+                // Update Game at 60fps
+                await Task.Delay(1);
+            }
+        }
+
+        /// <summary>
+        /// Stop the gameloop.
+        /// </summary>
+        public void Stop()
+        {
+            Running = false;
+            FPStimer.Stop();
+            _SkillIssue?.Unload();
+        }
+
+        /// <summary>
+        /// Update the FPS counter and reset FPS step every second.
+        /// </summary>
+        public void ResetFPS(Object source, ElapsedEventArgs e)
+        {
+            _SkillIssue.FPS = _SkillIssue.FPSstep;
+            _SkillIssue.FPSstep = 0;
+        }
+
+
+        /// <summary>
+        /// Render the graphic buffer and increment the FPS counter.
+        /// </summary>
+        public void Draw(Graphics gfx)
+        {
+            _SkillIssue.Draw(gfx);
+            _SkillIssue.FPSstep++;
+        }
+    }
+}
